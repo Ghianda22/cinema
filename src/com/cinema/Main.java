1706e431812f95cfbed8e7105f5027d875d09de4
Valentina <valelore98@gmail.com>
2022-10-06 09:37:34 +0200

    Defined the cost of the ticket based on user age

--- src/com/cinema/Main.java
+++ src/com/cinema/Main.java
@@ -1,5 +1,24 @@
 package com.cinema;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println("Quanti anni hai? ");
+		int userAge = in.nextInt();
+		int cost = 0;
+		
+		
+		if(userAge > 15) {
+			cost = 10;
+		}else if(userAge >= 7 && userAge < 15) {
+			cost = 6;
+		}
+		
+		System.out.println("Il tuo  biglietto " + (cost == 0 ? "e' gratis!" : String.format("costa %.2f €", (float)cost)));	
+		
+		in.close();
+	}
 }
